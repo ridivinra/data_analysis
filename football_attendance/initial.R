43d5e6543034fcf7c476e13d04b90b40ee5b360a
ridivinra <arni.vidir.johannesson@gmail.com>
2020-02-15 16:43:02 +0000

    fist commit

--- football_attendance/initial.R
+++ football_attendance/initial.R
@@ -13,9 +13,13 @@ attend %>% left_join(games, by = c("year", "team_name_long" = "home_team", "week
 
 games_attendance <- attend %>% left_join(games, by = c("year", "team_name_long" = "home_team", "week")) %>% 
   filter(!is.na(weekly_attendance)) %>%
-  mutate(team_name_long = fct_reorder(team_name_long, weekly_attendance))
-games_attendance %>% ggplot(aes(team_name_long, weekly_attendance)) + geom_boxplot() + coord_flip()
-attend %>% left_join(games, by = c("year", "team_name_long" = "home_team", "week")) %>% 
-  filter(is.na(weekly_attendance))
+  mutate(
+    team_name_long = fct_reorder(team_name_long, weekly_attendance),
+    week = fct_reorder(factor(week), weekly_attendance),)
 
+games_attendance %>% ggplot(aes(team_name_long, log(weekly_attendance))) + geom_boxplot() + coord_flip()
+games_attendance %>% ggplot(aes(week, log(weekly_attendance))) + geom_boxplot() + coord_flip()
 
+games_attendance %>% ggplot(aes(log(weekly_attendance))) + geom_density()
+
+games_attendance %>% group_by(team_name_long) %>% 
